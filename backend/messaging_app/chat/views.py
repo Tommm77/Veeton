0550c72fe856b27b574fe953cd05d44fa1945b0c
Tommm77 <picout.tom@gmail.com>
2024-05-18 16:44:28 +0200

    create base of project

---
+++ backend/messaging_app/chat/views.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets
+from .models import ChatRoom, Message
+from .serializer import ChatRoomSerializer, MessageSerializer
+
+
+class ChatRoomViewSet(viewsets.ModelViewSet):
+    queryset = ChatRoom.objects.all()
+    serializer_class = ChatRoomSerializer
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
